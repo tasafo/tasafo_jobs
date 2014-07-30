f6846a697862f992eb2cc545dc3daee7bbd0e375
Felipe Iketani <felipeiketani@gmail.com>
2014-07-30 20:59:22 -0300

    1st design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def tab_active?(link)
+    "active" if link.to_s == controller_name.to_s && action_name == "index"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 TasafoJobs::Application.routes.draw do
+  get 'jobs/show'
+
+  root 'home#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
