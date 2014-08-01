f00e921abe03db5ff0aaeda48d61bd355e37dc9b
Felipe Iketani <felipeiketani@gmail.com>
2014-08-01 14:12:51 -0300

    added resume validations

--- spec/models/resume_spec.rb
+++ spec/models/resume_spec.rb
@@ -4,6 +4,10 @@ describe Resume do
   fixtures :resumes
 
   describe "Validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:category) }
+    it { should validate_presence_of(:user) }
   end
 
   describe "scopes" do
